f7916159f0362ebdbf5562b9d0a6b3e63470412e
haythamac <janssen.uy.dev@gmail.com>
2026-05-09 05:44:38 +0800

    chore: added simple booking view layout without design

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,7 +1,24 @@
 <script setup>
+import { ref } from 'vue';
 
+const currentStep = ref(1);
 </script>
 
 <template>
     <h1>Booking View</h1>
+    <div>
+        <div v-if="currentStep === 1">
+            <h2>Step 1</h2>
+        </div>
+        <div v-else-if="currentStep === 2">
+            <h2>Step 2</h2>
+        </div>
+        <div v-else>
+            <h2>Step 3</h2>
+        </div>
+    </div>
+    <div>
+        <button @click="currentStep--" :disabled="currentStep === 1">Back</button>
+        <button @click="currentStep++" :disabled="currentStep === 3">Next</button>
+    </div>
 </template>
