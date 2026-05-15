251fac4224922342f1f4579aac81dd25acbb6b7f
haythamac <janssen.uy.dev@gmail.com>
2026-05-15 21:27:36 +0800

    chore: finish bookingview design (on step 2)

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -93,46 +93,90 @@ function confirmButton() {
 </script>
 
 <template>
-    <h1>Booking View</h1>
-    <div>
-        <div v-if="currentStep === 1">
-            <h2>Step 1</h2>
-            <h1>ha</h1>
-            <VDatePicker v-model="bookingDate" mode="dateTime" />
-            <p v-if="errors.bookingDate" class="text-red-500 text-sm">{{ errors.bookingDate }}</p>
+    <div class="mx-auto max-w-4xl my-10 p-5 border border-amber-400 rounded-4xl shadow-md">
+        <div class="flex justify-between mb-8 my-4">
+            <div class="flex gap-2">
+                <p class="bg-green-700 rounded-full w-8 h-8 flex items-center justify-center mx-auto"></p>
+                <h1 class="text-2xl">Booking View</h1>
+            </div>
+            <h1>Step 1 of 3</h1>
         </div>
-        <div v-else-if="currentStep === 2">
-            <h2>Step 2</h2>
-            <form>
-                <label for="customer_name">Name:</label>
-                <input type="text" id="customer_name" v-model="form.customer_name" />
-                <p v-if="errors.customer_name" class="text-red-500 text-sm">{{ errors.customer_name }}</p>
 
-                <label for="customer_email">Email:</label>
-                <input type="email" id="customer_email" v-model="form.customer_email" />
-                <p v-if="errors.customer_email" class="text-red-500 text-sm">{{ errors.customer_email }}</p>
+        <div class="flex justify-between items-center">
+            <div class="flex gap-2 items-center">
+                <p class="bg-green-700 rounded-full w-8 h-8 flex items-center justify-center mx-auto text-white">1</p>
+                <p class="flex items-center justify-center mx-auto">Date & Time</p>
+            </div>
 
-                <label for="customer_phone">Phone:</label>
-                <input type="tel" id="customer_phone" v-model="form.customer_phone" />
-                <p v-if="errors.customer_phone" class="text-red-500 text-sm">{{ errors.customer_phone }}</p>
+            <span class="flex-1 border-t border-gray-300 mx-4"></span>
 
-                <label for="notes">Notes:</label>
-                <textarea id="notes" v-model="form.notes"></textarea>
-            </form>
+            <div class="flex gap-2 items-center">
+                <p
+                    class="bg-white-700 border border-gray-300 rounded-full w-8 h-8 flex items-center justify-center mx-auto">
+                    2</p>
+                <p class="flex items-center justify-center mx-auto">Your Details</p>
+            </div>
+
+            <span class="flex-1 border-t border-gray-300  mx-4"></span>
+
+            <div class="flex gap-2 items-center">
+                <p
+                    class="bg-white-700 border border-gray-300 rounded-full w-8 h-8 flex items-center justify-center mx-auto">
+                    3</p>
+                <p class="flex items-center justify-center mx-auto">Confirm</p>
+            </div>
         </div>
-        <div v-else>
-            <h2>Step 3</h2>
-            <div>Summary of booking: </div>
-            <p><strong>Date:</strong> {{ bookingDate.toLocaleString() }}</p>
-            <p><strong>Name:</strong> {{ form.customer_name }}</p>
-            <p><strong>Email:</strong> {{ form.customer_email }}</p>
-            <p><strong>Phone:</strong> {{ form.customer_phone }}</p>
-            <p><strong>Notes:</strong> {{ form.notes }}</p>
+
+        <div class="mt-10 gap-5 mx-auto max-w">
+            <div v-if="currentStep === 1">
+                <h2 class="text-3xl mb-4">Choose your time</h2>
+                <p class="mb-4 text-gray-700">Select a date and time for your booking.</p>
+                <VDatePicker v-model="bookingDate" mode="dateTime" />
+                <p v-if="errors.bookingDate" class="text-red-500 text-sm">{{ errors.bookingDate }}</p>
+            </div>
+            <div v-else-if="currentStep === 2">
+                <h2 class="text-3xl mb-4">Your Details</h2>
+                <p class="mb-4 text-gray-700">We'll use this to confirm your appointment.</p>
+                <form class="flex flex-col gap-4">
+                    <div class="flex justify-between items-center">
+                        <div class="flex flex-col">
+                            <label for="customer_name">Name</label>
+                            <input type="text" id="customer_name" v-model="form.customer_name" />
+                            <p v-if="errors.customer_name" class="text-red-500 text-sm">{{ errors.customer_name }}</p>
+                        </div>
+
+                        <div>
+                            <label for="customer_phone">Phone</label>
+                            <input type="tel" id="customer_phone" v-model="form.customer_phone" />
+                            <p v-if="errors.customer_phone" class="text-red-500 text-sm">{{ errors.customer_phone }}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="customer_email">Email</label>
+                        <input type="email" id="customer_email" v-model="form.customer_email" />
+                        <p v-if="errors.customer_email" class="text-red-500 text-sm">{{ errors.customer_email }}</p>
+                    </div>
+                    <label for="notes">Notes (Optional)</label>
+                    <textarea id="notes" v-model="form.notes"></textarea>
+                </form>
+            </div>
+            <div v-else>
+                <h2>Step 3</h2>
+                <div>Summary of booking: </div>
+                <p><strong>Date:</strong> {{ bookingDate.toLocaleString() }}</p>
+                <p><strong>Name:</strong> {{ form.customer_name }}</p>
+                <p><strong>Email:</strong> {{ form.customer_email }}</p>
+                <p><strong>Phone:</strong> {{ form.customer_phone }}</p>
+                <p><strong>Notes:</strong> {{ form.notes }}</p>
+            </div>
         </div>
-    </div>
-    <div>
-        <button @click="currentStep--" :disabled="currentStep === 1">Back</button>
-        <button @click="nextStep" v-if="currentStep !== 3">Next</button>
-        <button @click="confirmButton" v-if="currentStep === 3">Confirm</button>
+        <div class="flex justify-between items-center mt-8">
+            <button class="cursor-pointer border border-gray-300 flex items-center rounded-full py-2 px-6"
+                @click="currentStep--" :disabled="currentStep === 1">Back</button>
+            <button class="cursor-pointer border bg-green-700 text-white flex items-center rounded-full py-2 px-6"
+                @click="nextStep" v-if="currentStep !== 3">Next</button>
+            <button class="cursor-pointerborder bg-green-700 text-white flex items-center rounded-full py-2 px-6"
+                @click="confirmButton" v-if="currentStep === 3">Confirm</button>
+        </div>
     </div>
 </template>
